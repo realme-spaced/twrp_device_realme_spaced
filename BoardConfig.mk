dba755830674a38cccd86862988852e7c70ca12c
HELLINFIX <mridulsharma57570@gmail.com>
2023-09-03 08:52:31 +0000

    spaced: PBRP specific build flags - Disable treble compatibility - Add flash light node

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,3 +153,7 @@ TW_H_OFFSET := -115
 # Debug
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
+
+# PBRP specific build flags
+PB_DISABLE_DEFAULT_TREBLE_COMP := true
+PB_TORCH_PATH := "/sys/class/flashlight"
